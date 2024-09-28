527683ccb23fef666909f4c10ec7aa37be8ceeca
Puranjot Singh <puranjotsinghnaga@gmail.com>
2024-09-29 00:44:37 +0530

    changed Hero section

--- code/src/components/Hero.jsx
+++ code/src/components/Hero.jsx
@@ -11,35 +11,29 @@ const Hero = () => {
       <div>
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
-          fill="black"  
+          fill="black"
         />
         <Spotlight
           className="top-10 left-full h-[80vh] w-[50vw]"
-          fill="white"  
+          fill="white"
         />
-        <Spotlight
-          className="top-28 left-80 h-[80vh] w-[50vw]"
-          fill="white"  
-        />
+        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="white" />
       </div>
-      <div className="flex items-center justify-center absolute inset-0">
-        <div className="flex flex-col items-center justify-center relative z-10 h-full text-center">
-          <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw]">
-            <h2 className="tracking-widest my-5 text-4xl font-extrabold text-blue-100">
-              INFRA MONITORING TOOL
-            </h2>
-            <ProdDesc />
-            <Meet/>
-            {/* <a href="#about" className="mt-8">
+      <div className="flex items-center justify-center pt-20 flex-col">
+        <h1 className="text-white text-[5rem] font-semibold ">Pandora</h1>
+        <h2 className="text-slate-300 text-[3rem] font-thin mt-[-2rem]">
+          The Efficient Data Log Manager
+        </h2>
+      </div>
+      <ProdDesc />
+      <Meet />
+      {/* <a href="#about" className="mt-8">
               <MagicButton
                 title="About our Project"
                 icon={<RiArrowDropDownLine />}
                 position="right"
               />
             </a> */}
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
--- code/src/components/Navbar.jsx
+++ code/src/components/Navbar.jsx
@@ -1,35 +1,67 @@
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  DropdownItem,
+  DropdownTrigger,
+  Dropdown,
+  DropdownMenu,
+  Avatar,
+} from "@nextui-org/react";
 
 export default function App() {
   return (
-    <Navbar position="static">
+    <Navbar shouldHideOnScroll>
       <NavbarBrand>
         <p className="font-bold text-inherit">Pandora</p>
       </NavbarBrand>
-      {/* <NavbarContent className="hidden sm:flex gap-4" justify="center">
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarItem>
           <Link color="foreground" href="#">
-            Home
+            home
           </Link>
         </NavbarItem>
         <NavbarItem isActive>
-          <Link href="#" aria-current="page">
-          Project Desc
+          <Link href="#" aria-current="page" color="primary">
+            features
           </Link>
         </NavbarItem>
         <NavbarItem>
           <Link color="foreground" href="#">
-          Meet the Team
+            contact us
           </Link>
         </NavbarItem>
-      </NavbarContent> */}
-      <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="/sign-in">Login</Link>
-        </NavbarItem>
-        <NavbarItem>
-        <Link href="/sign-up">Sign Up</Link>
-        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent as="div" justify="end">
+        <Dropdown placement="bottom-end">
+          <DropdownTrigger>
+            <Avatar
+              isBordered
+              as="button"
+              className="transition-transform"
+              color="secondary"
+              name="Jason Hughes"
+              size="sm"
+              src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+            />
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Profile Actions" variant="flat">
+            <DropdownItem key="profile" className="h-14 gap-2">
+              <p className="font-semibold">Signed in as</p>
+              <p className="font-semibold">zoey@example.com</p>
+            </DropdownItem>
+            <DropdownItem key="settings">Home</DropdownItem>
+            <DropdownItem key="team_settings">Features</DropdownItem>
+            <DropdownItem key="analytics">Contact Us</DropdownItem>
+            <DropdownItem key="logout" color="danger">
+              Log Out
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
       </NavbarContent>
     </Navbar>
   );
